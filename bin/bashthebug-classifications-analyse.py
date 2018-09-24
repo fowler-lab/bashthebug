84d3db9348c12f77a0b3d46c61e62426a1b2280f
Philip Fowler <philipwfowler@gmail.com>
2018-09-24 21:36:27 +0100

    find out most recent date from the CSV file

--- bin/bashthebug-classifications-analyse.py
+++ bin/bashthebug-classifications-analyse.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import time, argparse, logging
+import argparse, logging
 
 import pandas
 
@@ -10,46 +10,52 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_file",required=True,help="the csv file downloaded from the Zooniverse containing all the classifcations done to date")
-    parser.add_argument("--output_stem",default="test",help="the first part of each of the output files")
+    parser.add_argument("--from_date",required=False,help="the date to consider classifications from (ISO format e.g. 2017-04-07)")
+    parser.add_argument("--to_date",required=False,help="the date to consider classifications up to")
     parser.add_argument("--timings",action='store_true',default=False,help="print the time taken for each step")
     options = parser.parse_args()
 
     # parse the output file to work out the output stem
     output_stem=options.input_file.split("bash-the-bug-classifications")[1].split(".csv")[0]
 
-    # open a log file to record images where the wells cannot be identified
-    logging.basicConfig(filename="log/btb-classifications-analyse-"+output_stem+".log",level=logging.INFO,format='%(levelname)s: %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
 
     print("Reading classifications from CSV file...")
-    start=time.time()
-    current_classifications=bashthebug.BashTheBugClassifications(zooniverse_file=options.input_file)
-    if options.timings:
-        print("%.1f seconds" % (time.time()-start))
+
+    if options.to_date:
+        if options.from_date:
+            current_classifications=bashthebug.BashTheBugClassifications(zooniverse_file=options.input_file,to_date=options.to_date,from_date=options.from_date)
+        else:
+            current_classifications=bashthebug.BashTheBugClassifications(zooniverse_file=options.input_file,to_date=options.to_date)
+    elif options.from_date:
+        current_classifications=bashthebug.BashTheBugClassifications(zooniverse_file=options.input_file,from_date=options.from_date)
+    else:
+        current_classifications=bashthebug.BashTheBugClassifications(zooniverse_file=options.input_file)
+        
 
     current_classifications.extract_classifications()
+
+    most_recent_date=str(current_classifications.classifications.created_at.max().date().isoformat())
+
+    # open a log file to record images where the wells cannot be identified
+    logging.basicConfig(filename="log/bashthebug-classifications-analyse-"+most_recent_date+".log",level=logging.INFO,format='%(levelname)s: %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
 
     current_classifications.create_measurements_table()
 
     current_classifications.create_users_table()
 
-    start=time.time()
+
     for sampling_time in ['month','week','day']:
 
-        current_classifications.plot_classifications_by_time(sampling=sampling_time,filename='pdf/'+output_stem+'-classifications-'+sampling_time+'.pdf',add_cumulative=True)
-        current_classifications.plot_users_by_time(sampling=sampling_time,filename='pdf/'+output_stem+'-users-'+sampling_time+'.pdf',add_cumulative=True)
+        current_classifications.plot_classifications_by_time(sampling=sampling_time,filename='pdf/graph-classifications-'+sampling_time+'.pdf',add_cumulative=True)
+        current_classifications.plot_users_by_time(sampling=sampling_time,filename='pdf/graph-users-'+sampling_time+'.pdf',add_cumulative=True)
 
-    current_classifications.plot_user_classification_distribution(filename="pdf/"+output_stem+'-user-distribution.pdf')
+    current_classifications.plot_user_classification_distribution(filename="pdf/graph-user-distribution.pdf")
 
-    if options.timings:
-        print("%.1f seconds" % (time.time()-start))
 
     logging.info(current_classifications)
 
     print("Saving compressed CSV file...")
-    start=time.time()
     current_classifications.save_csv("dat/bash-the-bug-classifications.csv.bz2",compression=True)
-    if options.timings:
-        print("%.1f seconds" % (time.time()-start))
 
     logging.info(current_classifications.users[["classifications","rank"]][:20])
 
